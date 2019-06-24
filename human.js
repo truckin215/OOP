8bef6a4069dfbf11b44a2d1d730c439c31837f78
Connor_Murray <connormurray215@gmail.com>
2019-06-24 13:39:15 -0400

    Push

---
+++ human.js
@@ -0,0 +1,17 @@
+function display(content){
+    console.log(content)
+}
+
+// define function
+
+
+// define parent
+display(sandy.sleep())
+display(shawn)
+
+
+// child class
+// instantiate yourself from the child class
+
+display(connor.videogames())
+display(connor.sleep())
